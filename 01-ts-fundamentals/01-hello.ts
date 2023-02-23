3a8245a75d80ef1d565803cadf76137b0fb039d5
edmundo <edcossio9@gmail.com>
2023-02-23 19:42:52 -0400

    Second commit!

---
+++ 01-ts-fundamentals/01-hello.ts
@@ -0,0 +1,2 @@
+console.log("Hello world");
+//npx ts-node .\01-ts-fundamentals\01-hello.ts
---
+++ 01-ts-fundamentals/02-variable.ts
@@ -0,0 +1,11 @@
+let var1 = true;
+console.log(var1)
+
+var1 =false;
+console.log(var1);
+
+const var2 = "text";
+//var2 = "tes";
+
+//var var3 = 123;
+//var3 = 44;
